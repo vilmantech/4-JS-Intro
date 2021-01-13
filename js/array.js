5f933e0a5366873bb15178629d5db1873e598c4e
Vilmante <vilmantech@yahoo.com>
2021-01-13 16:56:08 +0200

    array

---
+++ js/array.js
@@ -0,0 +1,28 @@
+// Array- bet kokiu elementu sarasas (prioriteteas to paties tipo elemntai)
+
+const tusciapinigine = [];
+console.log(tusciapinigine);
+
+const pinigine = [1, 2, 5, 5, 20];
+console.log(pinigine);
+
+const abc =['a', 'b', 'c', 'd', 'e'];
+console.log(abc);
+
+// kaip suzinoti kiek sare yra reiksmiu: array.length
+console.log('kiek yra reiksmiu tusciame sarase:', tusciapinigine.length);
+console.log('kiek yra reiksmiu pinigineje:', pinigine.length);
+
+// kaip suzinoti koks teksto ilgis: tekstas.length
+const labas = 'labas';
+const labasilgis = labas.length;
+console.timeLog(`"${labas}" ilgis yra ${labasilgis}.`);
+
+// istraukti saraso reiksme
+const pirmaabc = abc[0];
+console.log('pirma raide', pirmaabc);
+
+// istraukti teksto n-taji simboli
+const hi = 'labas';
+const pirmaraide = hi[0];
+console.log(pirmaraide);
