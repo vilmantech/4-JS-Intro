2286eccde571329532d6389aa7490efe52b3d035
Vilmante <vilmantech@yahoo.com>
2021-01-14 16:08:01 +0200

    ternary

---
+++ js/ternary.js
@@ -0,0 +1,26 @@
+const a = 4;
+const b = 4;
+
+const arlygu = a === b ? true : false;
+console.log(arlygu);
+
+
+const aukstai = 2;
+const kadadaugiaaukstis = 5;
+
+const ardaugiaaukstis = aukstai >= kadadaugiaaukstis ? 'taip' : 'ne';
+console.log(ardaugiaaukstis);
+
+
+const varyklisijungtas = false;
+const arjudam = varyklisijungtas === true ? 'vaziuojam' : 'stovim';
+console.log(arjudam);
+
+
+// bus egze kazkas tokio:
+const skaicius = true 
+                    ? true 
+                        ? 4 
+                        : 3 
+                        : 2;
+console.log(skaicius);
