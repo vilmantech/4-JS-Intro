28e33cede1ef610f2d8477272f8eedeecc2d7d63
Vilmante <vilmantech@yahoo.com>
2021-01-18 15:27:24 +0200

    reverse

---
+++ js/reverse.js
@@ -0,0 +1,37 @@
+"use strict";
+// perrasyti teksta is galo i prieki
+
+const text = 'abcdefg';
+const size = text.length;
+let reversetext = ' ';
+
+// for (let i = 0; i < text.length; i++) {
+//     const letter = text[i];
+//     reversetext = letter + reversetext;
+//     console.log(i, letter, reversetext);
+// }
+
+// //  for (let i = 0; i < size; i++) {
+//     const letter = text[size - i - 1];
+//     reversetext += letter;
+//     console.log(i, letter, reversetext);
+// }
+
+// for (let i = 1; i < size; i++) {
+//     const letter = text[size - i];
+//     reversetext += letter;
+//     console.log(i, letter, reversetext);
+// }
+
+// for (let i = size - 1; i >= 0; i--) {
+//     const letter = text[i];
+//     reversetext += letter;
+//     console.log(i, letter, reversetext);
+// }
+
+for (let i = size; i > 0; i--) {
+    const letter = text[i - 1];
+    reversetext += letter;
+    console.log(i, letter, reversetext);
+}
+console.log('finale gavau:', reversetext);
