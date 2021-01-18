29c68dd3d848e40316160988f925c491a56fb9c0
Vilmante <vilmantech@yahoo.com>
2021-01-18 16:19:55 +0200

    sum

---
+++ js/sum.js
@@ -0,0 +1,14 @@
+const n1 = 2;
+const n2 = 5;
+
+const n3 = 22;
+const n4 = 55;
+
+function sum(firstnumber, secondnumber) {
+    const totalsum = firstnumber + secondnumber
+    console.log(totalsum);
+    return totalsum;
+}
+const s1 = sum(9, 4);
+const s2 = sum(n1, n2);
+const s3 = sum(n3, n4);
